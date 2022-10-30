c196d0b9de588dfd546d6a5a2e391c4faf908c4a
Juliya Boiko <boikojuliya2707@gmail.com>
2022-10-31 01:11:43 +0400

    init

---
+++ App.js
@@ -0,0 +1,62 @@
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+import { StatusBar } from 'expo-status-bar';
+import { useCallback, useEffect, useState } from 'react';
+import { StyleSheet, View, Platform } from 'react-native';
+import { RegistrationScreen } from './screens/auth/RegistrationScreen';
+import { LoginScreen } from './screens/auth/LoginScreen';
+
+SplashScreen.preventAutoHideAsync();
+
+const loadApplication = async () => {
+  await Font.loadAsync({
+    "Roboto-400": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-500": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-700": require("./assets/fonts/Roboto-Bold.ttf")
+  });
+};
+
+export default function App() {
+  console.log(Platform.OS);
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await loadApplication();
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      {/* <RegistrationScreen /> */}
+      <LoginScreen />
+      <StatusBar style="dark" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
---
+++ screens/auth/RegistrationScreen.js
@@ -0,0 +1,124 @@
+import { TouchableWithoutFeedback, Keyboard, ImageBackground, KeyboardAvoidingView, View, Text, TextInput,
+  TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import { useState, useEffect } from 'react';
+
+const initialState = {
+  login: '',
+  email: '',
+  password: ''
+};
+
+export const RegistrationScreen = () => { 
+  const [isKeyboardShown, setIsKeyboardShown] = useState(false);
+  const [userState, setUserState] = useState(initialState);
+  const [dimensions, setDimensions] = useState(Dimensions.get('window').width);
+
+  useEffect(() => {
+    const onChange = () => {
+      const width = Dimensions.get('window').width;
+      setDimensions(width);
+    };
+    const width = Dimensions.addEventListener('change', onChange);
+    return () => width?.remove();
+  });
+  
+  const keyboardHide = () => {
+    setIsKeyboardShown(false);
+    Keyboard.dismiss();
+  };
+
+  const submitHandler = () => {
+    keyboardHide();
+    console.log(userState);
+    setUserState(initialState);
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={keyboardHide}>
+      <ImageBackground style={styles.image} source={require('../../assets/images/bg-register.jpg')}>
+        <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
+          <View style={{ ...styles.form, paddingBottom: isKeyboardShown ? 32 : 78, paddingHorizontal: dimensions > 500 ? 60 : 16 }}>
+            <Text style={styles.title}>Реєстрація</Text>
+            <TextInput
+              placeholder='Логін'
+              placeholderTextColor='#BDBDBD'
+              style={styles.input}
+              onFocus={() => setIsKeyboardShown(true)}
+              value={userState.login}
+              onChangeText={(value) => setUserState((prevState) => ({ ...prevState, login: value }))} />
+            <TextInput
+              placeholder='Адреса електронної пошти'
+              placeholderTextColor='#BDBDBD'
+              style={styles.input}
+              onFocus={() => setIsKeyboardShown(true)}
+              value={userState.email}
+              onChangeText={(value) => setUserState((prevState) => ({ ...prevState, email: value }))} />
+            <TextInput
+              placeholder='Пароль'
+              placeholderTextColor='#BDBDBD'
+              secureTextEntry={true}
+              style={{ ...styles.input, marginBottom: 43 }}
+              onFocus={() => setIsKeyboardShown(true)}
+              value={userState.password}
+              onChangeText={(value) => setUserState((prevState) => ({ ...prevState, password: value }))} />
+            <TouchableOpacity style={styles.btn} onPress={submitHandler}>
+              <Text style={styles.btnText}>Зареєструватися</Text>
+            </TouchableOpacity>
+             <Text style={styles.link}>Вже є акаунт? Увійти</Text>
+          </View>
+        </KeyboardAvoidingView>
+      </ImageBackground>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'flex-end',
+  },
+  form: {
+    paddingTop: 92,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontFamily: 'Roboto-700',
+    fontSize: 30,
+    lineHeight: 35,
+    textAlign: 'center',
+    color: '#212121',
+    marginBottom: 32
+  },
+  input: {
+    padding: 10,
+    marginBottom: 16,
+    fontFamily: 'Roboto-400',
+    fontSize: 16,
+    borderWidth: 1,
+    borderColor: '#E8E8E8',
+    backgroundColor: '#F6F6F6',
+    borderRadius: 6,
+    color: '#212121',
+  },
+  btn: {
+    marginBottom: 16,
+    paddingVertical: 16,
+    borderRadius: 100,
+    backgroundColor: '#FF6C00',
+  },
+  btnText: {
+    fontFamily: 'Roboto-400',
+    fontSize: 16,
+    textAlign: 'center',
+    color: '#fff',
+  },
+  link: {
+    textAlign: 'center',
+    fontFamily: 'Roboto-400',
+    fontSize: 16,
+    color: '#1B4371',
+  }
+});
